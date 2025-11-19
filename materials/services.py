357ff89dd0b6ed9b470c115e9ba3347c71ceea92
Nikita Danilov <danilovn1@mail.ru>
2025-11-19 19:05:09 +0500

    hm_5.1

--- materials/services.py
+++ materials/services.py
@@ -6,8 +6,23 @@ from users.models import Payment
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
+def create_stripe_product(course: Course):
+    """
+    Создает продукт (Product) в Stripe.
+    """
+    try:
+        stripe_product = stripe.Product.create(
+            name=f"Курс: {course.title}",
+            description=course.description,
+            metadata={'course_id': course.pk}
+        )
+        return stripe_product.id
+    except stripe.error.StripeError as e:
+        print(f"Ошибка Stripe при создании продукта: {e}")
+        return None
 
-def create_stripe_price(course: Course):
+
+def create_stripe_price(course: Course, product_id: str):
     """
     Создает цену (Price) в Stripe на основе данных курса.
     """
@@ -19,11 +34,7 @@ def create_stripe_price(course: Course):
         stripe_price = stripe.Price.create(
             currency="usd",  # Используем доллары
             unit_amount=amount_in_cents,
-            # Создаем продукт на лету
-            product_data={
-                "name": f"Курс: {course.title}",
-            },
-            # Мод оплаты - разовый
+            product=product_id,
             type="one_time",
         )
 
@@ -38,55 +49,28 @@ def create_stripe_price(course: Course):
         return None
 
 
-def create_stripe_session(course_id: int, user):
+def create_stripe_session(price_id: str, user_email: str, client_ref_id: str):
     """
-    Создает сессию оплаты Checkout в Stripe для данного курса.
+    Создает сессию оплаты Checkout в Stripe для данного ID цены.
+    Не создает объекты в БД.
     """
-    try:
-        course = Course.objects.get(pk=course_id)
-    except Course.DoesNotExist:
-        return {"error": "Курс не найден."}, None
-
-    # Если ID цены Stripe еще не создан, создаем его
-    if not course.stripe_price_id:
-        price_id = create_stripe_price(course)
-        if not price_id:
-            return {"error": "Не удалось создать цену Stripe."}, None
-    else:
-        price_id = course.stripe_price_id
-
-    # Создание сессии оплаты
     try:
         checkout_session = stripe.checkout.Session.create(
-            # Цена и количество
             line_items=[
                 {
                     'price': price_id,
                     'quantity': 1,
                 },
             ],
-            # Режим оплаты
             mode='payment',
-            # URL для успешной и отмененной оплаты
-            success_url='http://127.0.0.1:8000/api/v1/payments/success?session_id={CHECKOUT_SESSION_ID}',
-            cancel_url='http://127.0.0.1:8000/api/v1/payments/cancel',
+            success_url='http://127.0.0.1:8000/api/v1/users/payments/success?session_id={CHECKOUT_SESSION_ID}',
+            cancel_url='http://127.0.0.1:8000/api/v1/users/payments/cancel',
 
-            # Добавляем ID пользователя для отслеживания
-            client_reference_id=str(user.pk),
-            customer_email=user.email,
+            client_reference_id=client_ref_id,
+            customer_email=user_email,
         )
 
-        # Создаем запись о платеже в базе данных
-        payment = Payment.objects.create(
-            user=user,
-            course=course,
-            amount=course.price,
-            payment_method=Payment.PaymentMethod.STRIPE,
-            stripe_session_id=checkout_session.id,
-            payment_link=checkout_session.url
-        )
-
-        return None, checkout_session.url
+        return None, checkout_session
 
     except stripe.error.StripeError as e:
         print(f"Ошибка Stripe при создании сессии: {e}")
